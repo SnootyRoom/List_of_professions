f27985a3afd2c97599111f32da8fafbe859d9fb5
SnootyRoom <126325976+SnootyRoom@users.noreply.github.com>
2024-03-27 14:25:21 +0500

    Ver 1.0

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def index():
     return render_template('base.html', title='Колонизация')
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
