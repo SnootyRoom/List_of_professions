5829c30ccec9e3c13144c8f2885c661032568945
SnootyRoom <126325976+SnootyRoom@users.noreply.github.com>
2024-03-27 16:24:37 +0500

    Ver 1.0

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -12,6 +14,13 @@ def index():
 @app.route('/training/<prof>')
 def training(prof):
     return render_template('training.html', prof=prof)
+
+
+@app.route('/list_prof/<list>')
+def professions(list):
+    with open("professions.json", "rt", encoding="utf8") as f:
+        professions_list = json.loads(f.read())
+    return render_template('professions.html', list=list, professions=professions_list)
 
 
 if __name__ == '__main__':
